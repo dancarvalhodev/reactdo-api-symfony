ff8c9dd2a5bd12a7777e294bea77afc9efb06ddd
Daniel Carvalho <danielcarvalhodev@gmail.com>
2021-08-10 14:06:18 -0300

    Bux Fix: Now is possible return all tasks

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -5,13 +5,11 @@ namespace App\Controller;
 use App\Entity\Task;
 use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Encoder\XmlEncoder;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Serializer;
+
 
 /**
  * @Route("/api/tasks", name="tasks.")
@@ -34,13 +32,17 @@ class TaskController extends AbstractController
     {
         $em = $this->getDoctrine()->getManager()->getRepository(Task::class);
         $tasks = $em->findAll();
+        $tasklist = [];
 
-        $encoders = [new XmlEncoder(), new JsonEncoder()];
-        $normalizers = [new ObjectNormalizer()];
-        $serializer = new Serializer($normalizers, $encoders);
+        foreach($tasks as $id => $task){
+            $tasklist[$id] = array(
+                'title' => $task->getTitle(),
+                'content' => $task->getContent(),
+                'created' => $task->getCreated(),
+            );
+        }
 
-        $jsonContent = $serializer->serialize($tasks, 'json');
-        return new Response($jsonContent);
+        return new JsonResponse($tasklist);
     }
 
     /**
